a5cbaeb01f2bb5bd5f1753c25762950963cb4064
mint-mints <772402444@qq.com>
2023-03-28 16:14:17 +0800

    [feat]upernet_mscan_base_fp16_224x224_pafpn_40k_crackdataset

---
+++ configs/_base_/models/upernet_mscan.py
@@ -0,0 +1,46 @@
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+custom_imports = dict(imports='mmcls.models', allow_failed_imports=False)
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segnext/mscan_b_20230227-3ab7d230.pth'  # noqa
+model = dict(
+    type='EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        type='MSCAN',
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file),
+        embed_dims=[64, 128, 320, 512],
+        mlp_ratios=[8, 8, 4, 4],
+        drop_rate=0.0,
+        drop_path_rate=0.1,
+        depths=[3, 3, 12, 3],
+        attention_kernel_sizes=[5, [1, 7], [1, 11], [1, 21]],
+        attention_kernel_paddings=[2, [0, 3], [0, 5], [0, 10]],
+        act_cfg=dict(type='GELU'),
+        norm_cfg=dict(type='BN', requires_grad=True)),
+    decode_head=dict(
+        type='UPerHead',
+        in_channels=[64, 128, 320, 512],
+        in_index=[0, 1, 2, 3],
+        pool_scales=(1, 2, 3, 6),
+        channels=512,
+        dropout_ratio=0.1,
+        num_classes=2,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    # auxiliary_head=dict(
+    #     type='FCNHead',
+    #     in_channels=512,
+    #     in_index=2,
+    #     channels=256,
+    #     num_convs=1,
+    #     concat_input=False,
+    #     dropout_ratio=0.1,
+    #     num_classes=19,
+    #     norm_cfg=norm_cfg,
+    #     align_corners=False,
+    #     loss_decode=dict(
+    #         type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
---
+++ configs/_local_model/upernet_mscan_base_fp16_224x224_pafpn_40k_crackdataset.py
@@ -0,0 +1,49 @@
+_base_ = [
+    '../_base_/models/upernet_mscan.py', '../_base_/datasets/crack_dataset.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+crop_size = (224, 224)
+model = dict(
+    decode_head=dict(type='UPerPAFPNHead',
+                     loss_decode=[dict(type='CrossEntropyLoss', use_sigmoid=False, loss_name='loss_ce', loss_weight=1.0),
+                                  dict(type='DiceLoss', loss_name='loss_dice', loss_weight=3.0)],
+                     dropout_ratio=0.7
+                     ),
+    # auxiliary_head=dict(in_channels=512, num_classes=2,
+    #                     loss_decode=[dict(type='CrossEntropyLoss', use_sigmoid=False, loss_name='loss_ce', loss_weight=1.0),
+    #                                  dict(type='DiceLoss', loss_name='loss_dice', loss_weight=3.0)],
+    #                     dropout_ratio=0.7
+    #                     ),
+    test_cfg=dict(mode='whole'),
+)
+
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.00006,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+
+lr_config = dict(
+    _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+data = dict(samples_per_gpu=2)
+# fp16 settings
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale='dynamic')
+# fp16 placeholder
+fp16 = dict()
+
