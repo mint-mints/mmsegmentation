e67908b2021b9c0a7ef7a280aac42d4b4209488d
mint-mints <772402444@qq.com>
2023-03-30 17:00:32 +0800

    [feat]添加backbone:regseg

---
+++ configs/_local_model/regseg_fp16_512x512_fpn_40k_crackdataset.py
@@ -0,0 +1,47 @@
+_base_ = [
+    '../_base_/models/regseg_fpn.py', '../_base_/datasets/crack_dataset.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+img_scale = (512, 512)
+crop_size = (512, 512)
+model = dict(
+    decode_head=dict(loss_decode=[dict(type='CrossEntropyLoss', use_sigmoid=False, loss_name='loss_ce', loss_weight=1.0),
+                                  dict(type='DiceLoss', loss_name='loss_dice', loss_weight=3.0)],
+                     dropout_ratio=0.7
+                     ),
+    # auxiliary_head=dict(loss_decode=[dict(type='CrossEntropyLoss', use_sigmoid=False, loss_name='loss_ce', loss_weight=1.0),
+    #                                  dict(type='DiceLoss', loss_name='loss_dice', loss_weight=3.0)],
+    #                     dropout_ratio=0.7
+    #                     ),
+)
+
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+
+lr_config = dict(
+    _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=0.95,
+    min_lr=1e-6,
+    by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+data = dict(samples_per_gpu=2)
+# fp16 settings
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale='dynamic')
+# fp16 placeholder
+fp16 = dict()
+
