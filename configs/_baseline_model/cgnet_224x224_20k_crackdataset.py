1d4569b1a54f655fd705849f7a5788c98bbd8449
mint-mints <772402444@qq.com>
2023-04-22 00:10:29 +0800

    [fix]修改一些细节错误

--- configs/_baseline_model/cgnet_224x224_20k_crackdataset.py
+++ configs/_baseline_model/cgnet_224x224_20k_crackdataset.py
@@ -5,7 +5,7 @@ _base_ = [
 
 crop_size = (224, 224)
 model = dict(
-    decode_head=dict(num_classes=2), auxiliary_head=dict(num_classes=2))
+    decode_head=dict(num_classes=2))
 
 # optimizer
 optimizer = dict(type='Adam', lr=0.0001, eps=1e-08, weight_decay=0.0005)
@@ -13,7 +13,7 @@ optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
-# total_iters = 60000
+total_iters = 20000
 checkpoint_config = dict(by_epoch=False, interval=4000)
 evaluation = dict(interval=4000, metric='mIoU')
 
